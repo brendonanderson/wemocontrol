0894e31d1d6cb727398d88e2157ae3ebced0b55f
Brendon Anderson <brendon.anderson@objectpartners.com>
2014-05-01 07:51:07 -0500

    Merge remote-tracking branch 'remotes/origin/multithread'
    Conflicts:
    	src/UpnpDiscovery.groovy
    	src/WemoDevice.groovy

--- src/UpnpDiscovery.groovy
+++ src/UpnpDiscovery.groovy
@@ -55,6 +55,7 @@ class UpnpDiscovery {
         recSocket.disconnect()
         recSocket.close()
     }
+
     private void processPacket(DatagramPacket packet) {
         String originaldata = new String(packet.data)
 //        log.info(originaldata)
@@ -77,7 +78,7 @@ class UpnpDiscovery {
             packet.append( "M-SEARCH * HTTP/1.1\r\n" )
             packet.append( "HOST: 239.255.255.250:1900\r\n" )
             packet.append( "MAN: \"ssdp:discover\"\r\n" )
-            packet.append( "MX: ").append( "3" ).append( "\r\n" )
+            packet.append( "MX: ").append( "2" ).append( "\r\n" )
             packet.append( "ST: " ).append( "ssdp:all" ).append( "\r\n" ).append( "\r\n" )
 //            packet.append( "ST: " ).append( "urn:Belkin:device:controllee:1" ).append( "\r\n" ).append( "\r\n" )
             byte[] data = packet.toString().bytes
